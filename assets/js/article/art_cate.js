9851da420895785fc2a19a3d998759186f738e42
tcf <1662380369@qq.com>
2021-01-20 22:08:42 +0800

    完成文章管理相关功能开发

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,100 @@
+$(function() {
+    var layer = layui.layer
+    var form = layui.form
+    var indexAdd = null
+    var indexEdit = null
+    initArtCateList()
+
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function(res) {
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+    $('#btnAddCate').on('click', function() {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        });
+
+    })
+    $('body').on('submit', '#form-add', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('添加失败')
+                }
+                layer.msg('添加成功')
+                initArtCateList()
+                layer.close(indexAdd)
+            }
+        })
+    })
+    $('tbody').on('click', '#btn-edit', function() {
+
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        });
+        var id = $(this).attr('data-id')
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates/' + id,
+            success: function(res) {
+
+                form.val('form-edit', res.data)
+            }
+        })
+    })
+
+    $('body').on('submit', '#form-edit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function(res) {
+
+                if (res.status !== 0) {
+                    return layer.msg('修改失败')
+                }
+                layer.msg('修改成功')
+                initArtCateList()
+                layer.close(indexEdit)
+            }
+        })
+    })
+    $('tbody').on('click', '#btnDel', function() {
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除失败')
+                    }
+                    layer.msg('删除成功')
+                    layer.close(index);
+                    initArtCateList()
+                }
+
+            })
+
+
+        });
+    })
+
+})
